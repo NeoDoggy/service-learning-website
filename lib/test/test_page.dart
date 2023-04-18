40c2409dcdfd821b1ddb7fb8ca0cc13651d724f4
an920107 <an920107@gmail.com>
2023-04-18 14:15:16 +0800

    串接 firebase 資料庫，登入時取得或上傳使用者資料

--- lib/test/test_page.dart
+++ lib/test/test_page.dart
@@ -11,11 +11,15 @@ class TestPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const WindowSize(),
-            const UserIcon(size: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                UserIcon(size: 32),
+              ],
+            ),
             MyMarkdown(mdContent),
+            const WindowSize(),
           ],
         ),
       ),
@@ -25,21 +29,29 @@ class TestPage extends StatelessWidget {
 
 
   final String mdContent = """
-  # 3/29 交通
+  # Linux 筆記
 
-  - 住宿地點 <-> 櫻島渡輪鹿兒島開航處
-      - 鹿兒島中央站 ==`坐公車(市16)`== 水族館前
-          - 約 25 分鐘
-          - ￥ 190
-      - 鹿兒島中央站 ==坐鹿兒島市電2系統== 水族館口
-          - 約 25 分鐘 (內含走路 10 分鐘)
-          - ￥ 170
+  ## 檔案結構
 
-  - 櫻島渡輪鹿兒島開航處 <-> 櫻島港渡輪碼頭
-    [https://www.city.kagoshima.lg.jp/sakurajima-ferry/koro-jikoku/timetable.html](https://www.city.kagoshima.lg.jp/sakurajima-ferry/koro-jikoku/timetable.html)
-    ![](https://i.imgur.com/RJMgWJd.png)
-      - 約 20 分鐘
-      - ￥ 200
+  - Linux 檔案結構是一個 tree，這個 tree 的 root 稱為 **根目錄 (/)**
+    ![](https://i.imgur.com/vyGJeTL.png)
+  - 在 Linux 上可以有很多使用者，包含 **一般使用者** 與 **超級管理員 (root)**
+  - 每個檔案都有一個擁有者、所屬群組，也可以個別設定擁有者、所屬群組、其他人的讀取、修改、執行權限
+  - 使用者可以有一個 **家目錄 (~)**，是打開終端機預設的初始位置，一般使用者的家目錄通常為 **/home/<使用者名稱>**，超級管理員的家目錄通常是 **/root**
+  - 表達檔案或資料夾位置可使用 **絕對路徑** 或 **相對路徑**
+    - 絕對路徑以 **根目錄 (/)** 出發，例如：`/var/www`、`/home/squid/Documents`
+    - 相對路徑會由參考目錄出發，常見的參考目錄有：
+      - 當前目錄 (.)
+      - 上一層目錄 (..)
+      - 家目錄 (~)
+      - 上一次處在的目錄 (-)
+    - 例如：`./Desktop`、`~/Documents/Programming`
+  - 在 Linux 中，以 **點 (.)** 開頭的檔案或目錄為隱藏檔案，例如：.vim、.bashrc，這些檔案是存在的，但 GUI 介面中看不到
+  - Windows 上直接以副檔名判別檔案類型，像是 txt 是文字檔、exe 是執行檔，更改附檔名會造成檔案無法開啟；但在 Linux 上，副檔名只是給使用者判別用的，他們本質上就是文字檔或二進制檔，像是一個 Python 檔，就算不打副檔名，一樣能透過 `python` 指令執行。
+  - **星號 ( * )** 為萬用字元，表示任意長度的任意字元，例如：
+    - *.cpp 表示所有 cpp 檔
+    - ./* 表示當前目錄下所有檔案
+
   
   ```
   #include <iostream>
--- lib/widgets/user_icon.dart
+++ lib/widgets/user_icon.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:service_learning_website/backend/user_data.dart';
 import 'package:service_learning_website/services/auth_service.dart';
 import 'package:service_learning_website/modules/icon_image.dart';
 
@@ -22,18 +24,23 @@ class UserIcon extends StatefulWidget {
 
 class _UserIconState extends State<UserIcon> {
 
+  final _auth = FirebaseAuth.instance;
+  final _userColl = FirebaseFirestore.instance.collection("users");
+
   bool _isHover = false;
 
   List<PopupMenuEntry<dynamic>> _menu = [];
   ImageProvider<Object>? _icon;
+  UserData? _userData;
 
   @override
   void initState() {
     super.initState();
     _menu = _generateMenu();
     _icon = _generateIcon();
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    _auth.authStateChanges().listen((User? user) {
       setState(() {
+        _updateUserData(user);
         _menu = _generateMenu();
         _icon = _generateIcon();
       });
@@ -77,20 +84,24 @@ class _UserIconState extends State<UserIcon> {
 
   List<PopupMenuEntry<dynamic>> _generateMenu() {
     // 有登入顯示的 menu
-    if (FirebaseAuth.instance.currentUser != null) {
+    if (_auth.currentUser != null) {
       return [
         PopupMenuItem(
           child: Row(
             children: [
-              Text(FirebaseAuth.instance.currentUser?.displayName ?? "<name>"),
+              Text(_auth.currentUser?.displayName ?? "<name>"),
               const SizedBox(width: 10),
-              Text(FirebaseAuth.instance.currentUser?.email ?? "<email>"),
+              Text(_auth.currentUser?.email ?? "<email>"),
             ],
           )
         ),
+        PopupMenuItem(child: Text("身份：${_userData?.permission.name ?? '<permission>'}")),
         const PopupMenuDivider(height: 10),
         PopupMenuItem(
-          onTap: () => FirebaseAuth.instance.signOut(),
+          onTap: () {
+            _auth.signOut();
+            // context.go(MyRouter.root);
+          },
           child: const Text("登出")
         ),
       ];
@@ -108,8 +119,8 @@ class _UserIconState extends State<UserIcon> {
 
   ImageProvider<Object> _generateIcon() {
     // 有登入顯示的 icon
-    if (FirebaseAuth.instance.currentUser != null) {
-      return NetworkImage(FirebaseAuth.instance.currentUser?.photoURL ?? "");
+    if (_auth.currentUser != null) {
+      return NetworkImage(_auth.currentUser?.photoURL ?? "");
     }
     // 沒登入顯示的 icon
     else {
@@ -120,4 +131,16 @@ class _UserIconState extends State<UserIcon> {
       );
     }
   }
+
+  void _updateUserData(User? user) {
+    if (user == null) return;
+    final uidDoc = _userColl.doc(user.uid);
+    uidDoc.snapshots().map((doc) => doc.data()).listen((map) {
+      _userData = (map == null) ?
+        UserData.fromUser(user) : UserData.fromJson(map);
+      _userData!.combine(UserData.fromUser(user));
+      uidDoc.set(_userData!.toJson());
+    });
+  }
 }
+
