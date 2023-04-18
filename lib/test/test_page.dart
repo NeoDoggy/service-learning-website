b9751c562ac7792bd64e17739faed262e1b60ba9
an920107 <an920107@gmail.com>
2023-04-18 21:47:20 +0800

    修改使用者頭像更新資料的方式

--- lib/test/test_page.dart
+++ lib/test/test_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:service_learning_website/test/window_size.dart';
+import 'package:service_learning_website/widgets/course_name_box.dart';
 import 'package:service_learning_website/widgets/my_markdown.dart';
 import 'package:service_learning_website/widgets/user_icon.dart';
 
@@ -18,6 +19,7 @@ class TestPage extends StatelessWidget {
                 UserIcon(size: 32),
               ],
             ),
+            const CourseNameBox(coursename: "test"),
             MyMarkdown(mdContent),
             const WindowSize(),
           ],
@@ -29,7 +31,23 @@ class TestPage extends StatelessWidget {
 
 
   final String mdContent = """
-  # Linux 筆記
+  # Markdown 測試
+
+  ## 服學規劃
+
+  |日期|項目|說明|
+  |---|---|---|
+  |04/18（二）|期中提案|- 需完成營隊所有內容，包含簡報、講義、營隊當天工作分配、報名表單、經費規劃，並於服學課程中報告與展示</br>- 網課與網站展示目前進度與成果，並報告詳細進度規劃|
+  |04/25（二）|AI, PY2 驗課|- 助教會再指定特定章節請講師演示</br>- 其他組休息；同時段另一組提供建議|
+  |05/02（二）|PY1, PY3 驗課|- 助教會再指定特定章節請講師演示</br>- 其他組休息；同時段另一組提供建議|
+  |05/09（二）|PY1, PY2, PY3 上機操作|- 實際操作營隊使用的電腦教室，熟悉設備</br>- 著重於程式碼實作的部分|
+  |05/13, 14（六、日）|PY1, PY2, PY3 營期第一週||
+  |05/16（二）|PY1, PY2, PY3 第一週檢討||
+  |05/20, 21（六、日）|PY1, PY2, PY3 營期第二週||
+  |05/23（二）|AI 二驗、行前會||
+  |05/27, 28（六、日）|AI 營期|去台中|
+  - 網課與網站組需每週另外約時間（或使用服學課程時間）與助教討論、確認進度
+
 
   ## 檔案結構
 
@@ -53,7 +71,7 @@ class TestPage extends StatelessWidget {
     - ./* 表示當前目錄下所有檔案
 
   
-  ```
+  ```cpp
   #include <iostream>
   using namespace std;
   int main() {
@@ -61,6 +79,5 @@ class TestPage extends StatelessWidget {
     return 0;
   }
   ```
-  
   """;
 }
