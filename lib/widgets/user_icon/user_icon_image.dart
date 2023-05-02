bd46d6c14cd203e102bc4d366cf3cc0a86e1de23
an920107 <an920107@gmail.com>
2023-05-02 16:10:30 +0800

    修復使用者頭像偶發性無法正常顯示

--- lib/widgets/user_icon/user_icon_image.dart
+++ lib/widgets/user_icon/user_icon_image.dart
@@ -1,10 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:service_learning_website/modules/icon_image.dart';
 import 'package:service_learning_website/providers/auth_provider.dart';
 
 class UserIconImage extends StatefulWidget {
-
   const UserIconImage({
     super.key,
     required this.size,
@@ -21,45 +23,53 @@ class UserIconImage extends StatefulWidget {
 }
 
 class _UserIconImageState extends State<UserIconImage> {
-
   bool _isHovered = false;
+  ImageProvider<Object>? _image;
+  Uint8List? _imageByte;
 
   @override
   Widget build(BuildContext context) {
-
-    final AuthProvider authProvider = Provider.of<AuthProvider>(context);
-    ImageProvider<Object> image;
-
-    // 沒登入顯示的 image
-    if (!authProvider.isAuthed || authProvider.userData?.photoURL == null) {
-      image = IconImage(
-          icon: Icons.person,
-          background: Colors.grey,
-          color: Colors.white
-      );
-    }
-    // 有登入顯示的 image
-    else {
-      image = NetworkImage(authProvider.userData!.photoURL);
-    }
-
     return MouseRegion(
         onEnter: (_) => setState(() => _isHovered = true),
         onExit: (_) => setState(() => _isHovered = false),
-        cursor: _isHovered ? SystemMouseCursors.click : SystemMouseCursors.basic,
+        cursor:
+            _isHovered ? SystemMouseCursors.click : SystemMouseCursors.basic,
         child: Container(
           decoration: BoxDecoration(
             border: Border.all(
-              color: _isHovered ? widget.hoverBorderColor : Colors.transparent,
-              width: widget.hoverBorderWidth
-            ),
+                color:
+                    _isHovered ? widget.hoverBorderColor : Colors.transparent,
+                width: widget.hoverBorderWidth),
             shape: BoxShape.circle,
           ),
-          child: CircleAvatar(
-            radius: widget.size,
-            foregroundImage: image,
+          child: Consumer<AuthProvider>(
+            builder: (context, authProvider, child) {
+              // 有登入顯示的 image
+              if (_imageByte == null && authProvider.isAuthed) {
+                http
+                    .get(Uri.parse(authProvider.userData!.photoURL))
+                    .timeout(const Duration(seconds: 5))
+                    .then((response) =>
+                        setState(() => _imageByte = response.bodyBytes))
+                    .catchError((_) => setState(() => _imageByte = null));
+              }
+              if (_imageByte != null) {
+                _image = MemoryImage(_imageByte!);
+              }
+              // 沒登入顯示的 image
+              else {
+                _image = IconImage(
+                    icon: Icons.person,
+                    background: Colors.grey,
+                    color: Colors.white);
+              }
+
+              return CircleAvatar(
+                radius: widget.size,
+                foregroundImage: _image,
+              );
+            },
           ),
-        )
-    );
+        ));
   }
 }
